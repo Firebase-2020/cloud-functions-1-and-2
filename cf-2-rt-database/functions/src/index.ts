5e310b0adb9f3acfc943d4cd459868ebdf6f0dbd
fotis tsakiris <footios76@gmail.com>
2020-03-31 17:15:01 +0300

    implemented onCreate and onDelete c. functions

--- cf-2-rt-database/functions/src/index.ts
+++ cf-2-rt-database/functions/src/index.ts
@@ -8,10 +8,10 @@ import * as functions from 'firebase-functions';
 // });
 
 /* 
-ref
+About `ref`
 1. The reference is very similar to other database references you might be using
 in your client code that also point to some location in the database.
-Here (in cloud functions) the ref points to the location that is mached 
+Here (in cloud functions) the ref points to the location that is matched 
 by the pattern ('/rooms/{roomId}/messages/{messageId}')
 that is given in the method definition.
 Use it to read/write info on that location. Or build more references to 
@@ -21,9 +21,41 @@ none of the security rulls can effect the way of reading and writing.
 */
 
 // This function needs the boilerplate where a chat dialog gets generated...
+// export  const onMessageCreate = functions.database
+// // {roomId} = this is a wildcard.
+// .ref('/rooms/{roomId}/messages/{messageId}').onCreate((snapshot, context) => {
+//     // use the event context to get the sting values of the wildcards.
+//     const roomId = context.params.roomId;
+//     const messageId = context.params.messageId;
+
+//     console.log(`New message ${messageId} in room ${roomId}`);
+
+//     // `val` give a copy of the data as a js obj
+//     const messageData = snapshot.val();
+//     const text =addPizzazz(messageData);
+//     return snapshot.ref.update({text: text})
+    
+// })
+
+
+export  const onMessageUpdate = functions.database
+.ref('/rooms/{roomId}/messages/{messageId}').onUpdate((change, context) => {
+  const before = change.before.val();
+  const after = change.after.val();
+  if (before.text === after.text) {
+      console.log('Text did not change');
+      return null;
+  }
+  const text = addPizzazz(after.text);
+  const timeEdited = Date.now();
+  // Use the ref to write the new text back to the data base.
+  return change.after.ref.update({text, timeEdited});
+})
+
+
 export  const onMessageCreate = functions.database
 // {roomId} = this is a wildcard.
-.ref('/rooms/{roomId}/messages/{messageId}').onCreate((snapshot, context) => {
+.ref('/rooms/{roomId}/messages/{messageId}').onCreate(async (snapshot, context) => {
     // use the event context to get the sting values of the wildcards.
     const roomId = context.params.roomId;
     const messageId = context.params.messageId;
@@ -32,11 +64,39 @@ export  const onMessageCreate = functions.database
 
     // `val` give a copy of the data as a js obj
     const messageData = snapshot.val();
-    const text =addPizzazz(messageData);
-    return snapshot.ref.update({text: text})
+    console.log('messageData', messageData);
     
+    const text = addPizzazz(messageData.text);
+    await snapshot.ref.update({text: text});
+
+    /* Build a ref to the location of the counter
+    From the `snapshot.ref` go up two nodes `parent.parent`.
+    Now we have a ref that points to `/rooms/{roomId}`.
+    Then we use the `child` method to create a messageCount child in that ref.
+
+    If you know from external means that an expression is not null or undefined, 
+    you can use the non-null assertion operator ! to coerce away those types:
+     */
+    const countRef = snapshot.ref.parent!.parent!.child('messageCount')
+    return countRef.transaction(count => {
+        return count + 1
+    })
 })
 
+
+export  const onMessageDelete = functions.database
+// {roomId} = this is a wildcard.
+.ref('/rooms/{roomId}/messages/{messageId}')
+.onDelete(async (snapshot, context) => {
+   
+    const countRef = snapshot.ref.parent!.parent!.child('messageCount')
+    return countRef.transaction(count => {
+        return count - 1
+    })
+})
+
+
 function addPizzazz(text: string): string {
-    return text.replace(/\bpizza\b/g, 'PIZZAZZ')
+    return text.replace(/\bpizza\b/g, 'PIZZAZZ');
 }
+
